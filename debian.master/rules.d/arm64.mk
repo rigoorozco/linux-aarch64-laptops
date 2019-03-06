44e8516db85344d83cb9ec1401563bdd19c41005
Lee Jones <lee.jones@linaro.org>
2019-03-06 11:55:27 +0000

    [UBUNTU PACKAGING] Ensure succesful building of the AArch64 Laptop kernel
     * Strip out unused Snapdragon flavour (causes build errors)
     * Copy Device Tree Binaries (DTBs) into /boot directory
     * Add CONFIGS required for smooth running on the Laptops
       * TODO: These are over-kill - need to strip them back
     * Bump upload version (as always) to keep LP happy
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- debian.master/rules.d/arm64.mk
+++ debian.master/rules.d/arm64.mk
@@ -9,18 +9,13 @@ install_file	= vmlinuz
 no_dumpfile = true
 uefi_signed     = true
 
-# The uboot used in ubuntu core can't handle Image.gz, so
-# create this flavour to generate a Image just for them
-build_image_snapdragon	= Image
-kernel_file_snapdragon	= arch/$(build_arch)/boot/Image
-
 loader		= grub
 vdso		= vdso_install
 
 do_extras_package = true
 do_tools_usbip  = true
 do_tools_cpupower = true
-do_tools_perf   = true
+do_tools_perf   = false
 
 do_dtbs		= true
 do_zfs		= true
